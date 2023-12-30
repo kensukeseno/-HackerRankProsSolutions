09ecbe732d795acc6fedc078489e8311767f7f62
ken <kensukemoneymoney@gmail.com>
2023-12-30 12:41:35 -0700

    update 20231230

---
+++ src/Algorithms/ChocolateFeast.java
@@ -0,0 +1,74 @@
+package Algorithms;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import javax.print.CancelablePrintJob;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ResultChocolateFeast {
+
+    /*
+     * Complete the 'chocolateFeast' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER c
+     *  3. INTEGER m
+     */
+
+    public static int chocolateFeast(int n, int c, int m) {
+    // Write your code here
+    	int chocolate = n /c;
+    	int wrapper = n /c;
+    	
+    	while(wrapper >= m) {
+    		int exchange = wrapper / m;
+    		chocolate += exchange;
+    		wrapper = wrapper - exchange * m + exchange;
+    	}
+    	
+    	return chocolate;
+    }
+}
+
+public class ChocolateFeast {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int c = Integer.parseInt(firstMultipleInput[1]);
+
+                int m = Integer.parseInt(firstMultipleInput[2]);
+
+                int result = ResultChocolateFeast.chocolateFeast(n, c, m);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
